b9e1b734acb4a701b04901c81dc32aa30e9e28ab
Aina <Aina@LAPTOP-721RF3FI>
2024-06-05 10:10:58 +0100

    Addition of Application layer and IRepository

---
+++ BookStore.Application/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using BookStore.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace BookStore.Application.Interfaces
+{
+    public interface IRepository<T> where T: Identifiable
+    {
+        Task<T> Get(string id);
+        Task<T> Get(Expression<Func<T, bool>> filter, string[]? includes = null);
+        Task<IEnumerable<T>> GetAll(string[]? includes = null, int count = 20);
+        Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>> filter, string[]? includes = null);
+        Task Add(T entity);
+        void Remove(T entity);
+        void Update(T entity);
+    }
+}
