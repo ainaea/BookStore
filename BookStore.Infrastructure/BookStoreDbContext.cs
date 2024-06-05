1a9d279e8c08dc575bbaa83f2f6e55b4f8f188d6
Aina <Aina@LAPTOP-721RF3FI>
2024-06-05 14:54:02 +0100

    Addition of BookStoreDb context

---
+++ BookStore.Infrastructure/BookStoreDbContext.cs
@@ -0,0 +1,32 @@
+﻿using BookStore.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Infrastructure
+{
+    public class BookStoreDbContext : IdentityDbContext<IdentityUser>
+    {        
+            public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options) : base(options)
+            { }
+
+            public DbSet<Author> Authors { get; set; }
+            public DbSet<Book> Books { get; set; }
+            public DbSet<Cart> Carts { get; set; }
+            public DbSet<CartedBook> CartedBooks { get; set; }
+            public DbSet<Genre> Genres { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+            {
+                base.OnModelCreating(builder);
+                //builder.Entity<OneLoan>().Property(o => o.InterestRate).HasColumnType("decimal(18,2)");
+                //builder.Entity<OneLoan>().Property(o => o.Amount).HasColumnType("decimal(18,2)");
+            }
+    }
+}
