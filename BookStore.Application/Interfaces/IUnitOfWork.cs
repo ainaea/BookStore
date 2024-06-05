da114c775dc25e4198ef688873b06cf2b26b450b
Aina <Aina@LAPTOP-721RF3FI>
2024-06-05 10:52:47 +0100

    Added IUnitOfWork to Application

---
+++ BookStore.Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using BookStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.Interfaces
+{
+    public interface IUnitOfWork: IDisposable
+    {
+        public IRepository<Book> Books { get; }
+        public IRepository<Author> Authors { get; }
+        public IRepository<Cart> Carts { get; }
+        public IRepository<CartedBook> CartedBooks { get; }
+        Task<int> Complete();
+    }
+}
