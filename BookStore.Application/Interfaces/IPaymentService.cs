ff62a1df1601ec5bdc72282e3ee32e00005e8728
Aina <Aina@LAPTOP-721RF3FI>
2024-06-06 16:10:09 +0100

    Added IPaymentService to Application

---
+++ BookStore.Application/Interfaces/IPaymentService.cs
@@ -0,0 +1,16 @@
+﻿using BookStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.Interfaces
+{
+    public interface IPaymentService
+    {
+        public Task<bool> WebPayment(Guid cartId, Decimal amount);
+        public Task<bool> USSDPayment(Guid cartId, Decimal amount);
+        public Task<bool> TransferPayment(Guid cartId, Decimal amount);
+    }
+}
