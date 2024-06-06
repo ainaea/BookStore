54a76d85900fa56512848e2cafd25abd33e986c8
Aina <Aina@LAPTOP-721RF3FI>
2024-06-06 08:45:52 +0100

    Addition of SqlConnectionFactory to Infrastructure

---
+++ BookStore.Infrastructure/SqlConnectionFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Infrastructure
+{
+    public class SqlConnectionFactory
+    {
+        private readonly IConfiguration config;
+
+        public SqlConnectionFactory(IConfiguration config)
+        {
+            this.config = config;
+        }
+        public SqlConnection CreateConnection()
+        {
+            return new SqlConnection(config.GetConnectionString("BookStoreConnection"));
+        }
+    }
+}
