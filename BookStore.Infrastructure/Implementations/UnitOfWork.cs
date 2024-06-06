71f50e81836ea5c0b6f08efb822f21c7300542dc
Aina <Aina@LAPTOP-721RF3FI>
2024-06-06 14:05:07 +0100

    Addition of Implementatio for UnitOfWork

---
+++ BookStore.Infrastructure/Implementations/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using BookStore.Application.Interfaces;
+using BookStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Infrastructure.Implementations
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IRepository<Book> Books { get;}
+
+        public IRepository<Author> Authors { get; }
+
+        public IRepository<Cart> Carts { get; }
+
+        public IRepository<CartedBook> CartedBooks { get; }
+        public UnitOfWork(IRepository<Book> books, IRepository<Author> authors, IRepository<Cart> carts, IRepository<CartedBook> cartedBooks)
+        {
+            Books = books;
+            Authors = authors;
+            Carts = carts;
+            CartedBooks = cartedBooks;
+        }
+
+        public Task<int> Complete()
+        {
+            return Task.FromResult(1);
+        }
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
