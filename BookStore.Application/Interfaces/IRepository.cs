01b838db01a7f1bb6a41bb55c34087b1b56c5f97
Aina <Aina@LAPTOP-721RF3FI>
2024-06-07 10:00:11 +0100

    Added optional  parameter to IRepository GetAll()

--- BookStore.Application/Interfaces/IRepository.cs
+++ BookStore.Application/Interfaces/IRepository.cs
@@ -9,7 +9,7 @@ namespace BookStore.Application.Interfaces
         T? Get(Expression<Func<T, bool>> filter, string[]? includes = null);
         IEnumerable<T>? GetAll(Expression<Func<T, bool>> filter, string[]? includes = null, int count = 20);
         //Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>> filter, string[]? includes = null);
-        IEnumerable<T>? GetAll();
+        IEnumerable<T>? GetAll(object[]? includes = null);
         Task Add(T entity);
         void Remove(T entity);
         void Update(T entity);
