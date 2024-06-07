c5b55ebd30986f0fa1d1bfe5bc4992d174a5b795
Aina <Aina@LAPTOP-721RF3FI>
2024-06-07 11:06:34 +0100

    Implementation of BookController

---
+++ BookStore.Presentation/Controllers/BookController.cs
@@ -0,0 +1,135 @@
+﻿using BookStore.Application.Interfaces;
+using BookStore.Domain.Entities;
+using BookStore.Presentation.DTOs;
+using BookStore.Presentation.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStore.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public BookController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(unitOfWork.Books.GetAll()?.Select(a => MapToBookDTO(a)));
+        }
+        [HttpPost]
+        public IActionResult AddBook([FromBody] BookDTO dto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(unitOfWork.Books.Add(MapToBook(dto, null)));
+        }
+        [HttpPost]
+        public IActionResult EditBook([FromBody] BookDTO dto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var book = unitOfWork.Books.Get(dto.Id);
+            if (book == null)
+                return NotFound($"Book with Id {dto.Id} not found");
+            unitOfWork.Books.Update(MapToBook(dto, book));
+            return Ok();
+        }
+        [HttpGet]
+        public IActionResult GetBook([FromQuery] Guid id)
+        {
+            var book = unitOfWork.Books.Get(id);
+            if (book == null)
+                return NotFound();
+            return Ok(MapToBookDTO(book));
+        }
+        [HttpPost]
+        [HttpGet]
+        public IActionResult GetBooksByAuthor([FromQuery] Guid id)
+        {
+            var author = unitOfWork.Authors.Get(id);
+            if (author == null)
+                return NotFound($"Author with Id {id} not found");
+            var book = unitOfWork.Books.GetAll(b => b.AuthorId == id);
+            if (book == null)
+                return NotFound();
+            return Ok( book.Select( b => MapToBookDTO(b)));
+        }
+
+        [HttpGet]
+        public IActionResult GetBooksByTitle([FromQuery] string title)
+        {
+            var book = unitOfWork.Books.GetAll(b => b.Title.ToLower().Contains(title.ToLower()));
+            if (book == null)
+                return NotFound();
+            return Ok(book.Select(b => MapToBookDTO(b)));
+        }
+        [HttpGet]
+        public IActionResult GetBooksByGenre([FromQuery] Guid id)
+        {
+            var genre = unitOfWork.Genres.Get(id);
+            if (genre == null)
+                return NotFound($"Genre with Id {id} not found");
+            var book = unitOfWork.Books.GetAll(b => b.GenreId == id);
+            if (book == null)
+                return NotFound();
+            return Ok(book.Select(b => MapToBookDTO(b)));
+        }
+
+        [HttpGet]
+        public IActionResult GetBooksByYear([FromQuery] int year)
+        {
+            var book = unitOfWork.Books.GetAll(b => b.Year == year);
+            if (book == null)
+                return NotFound();
+            return Ok(book.Select(b => MapToBookDTO(b)));
+        }
+
+        [HttpPost]
+        public IActionResult DeleteBook([FromBody] BookDTO dto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var book = unitOfWork.Books.Get(dto.Id);
+            if (book == null)
+                return NotFound($"Book with Id {dto.Id} not found");
+            unitOfWork.Books.Remove(MapToBook(dto, book));
+            return Ok();
+        }
+        public static Book MapToBook(BookDTO dto, Book? model)
+        {
+            //model should only be null during creation of new item
+            return new Book(dto.Name)
+            {
+                Id = model == null ? new Guid() :model.Id,
+                AuthorId = model == null ? dto.AuthorId : model.AuthorId,
+                GenreId = model == null ? dto.GenreId : model.GenreId,
+                ISBN = model == null ? dto.ISBN : model.ISBN,
+                Price = model == null ? dto.Price : model.Price,
+                Year = model == null ? dto.Year : model.Year,
+            };
+        }
+        public static BookDTO MapToBookDTO(Book model)
+        {
+            return new BookDTO
+            {
+                Id = model.Id,
+                AuthorId = model.AuthorId,
+                AuthorName = model.Author?.Name,
+                GenreId = model.GenreId,
+                GenreName = model.Genre?.Name,
+                Price = model.Price,
+                Name = model.Name,
+                Year = model.Year,
+                Title = model.Title           
+            };
+        }
+    }
+}
